6aa1c3b3b82d0dd09c5178059bf866a3403f1cbb
Luke Williams <luke@red-root.com>
2013-01-11 09:48:45 +0000

    Added to gh-pages

---
+++ gallery.coffee
@@ -0,0 +1,261 @@
+class Gallery
+  SETTINGS = 
+    thumbs:       true
+    thumb_width:  150
+    captions:     true
+    meta:         true
+    breakpoints:  
+      tablet: 940
+      mobile: 768
+    heights:
+      desktop: 550
+      tablet: 400
+      mobile: 250
+
+  constructor: (id, config={}) ->
+    @wrapper     = $(id)
+    @current     = @resolve_current()
+    @count       = @wrapper.find(".fmg-slide").length
+    @loading(true)
+    @init_bindings()
+    @resize()
+    @loading(false)
+    
+  slide_wrappers: -> @wrapper.find(".fmg-slides-wrapper, .fmg-captions-wrapper")
+  thumb_wrapper: -> @wrapper.find(".fmg-thumbs-wrapper").first()
+  
+  init_bindings: -> 
+    # bind clicks events
+    $(document).on "click", ".fmg-viewport-nav-left", => 
+      @slide_to(@current - 1)
+      false
+    $(document).on "click", ".fmg-viewport-nav-right", => 
+      @slide_to(@current + 1)
+      false
+    
+    # bind thumb click
+    $(document).on "click", ".fmg-thumbs-wrapper img", (e) =>
+      index = $(".fmg-thumbs-wrapper img").index(e.currentTarget);
+      @slide_to(index + 1);
+      false
+      
+    # bind full screen events
+    $(".fmg-fullscreen").remove() unless @has_fullscreen();
+    $(document).on "click", ".fmg-fullscreen", =>
+      @toggle_fullscreen()
+      false
+      
+    # bind thumbs toggle
+    $(document).on "click", ".fmg-toggle-thumbs", =>
+      @wrapper.find(".fmg-thumbs").toggleClass("is-hidden")
+      false
+      
+    # bind captions toggle
+    # bind thumbs toggle
+    $(document).on "click", ".fmg-toggle-captions", =>
+      @wrapper.find(".fmg-captions").toggleClass("is-hidden")
+      false
+    
+    # bind swipe event (hammer.js)
+
+    # bind keypress event
+    $(document).keydown (e) =>
+      if e.keyCode == 37 then @slide_to(@current - 1)
+      if e.keyCode == 39 then @slide_to(@current + 1)
+      
+    # bind resize event
+    window.onresize = => @resize()
+    
+  slide_to: (pos) ->
+    if pos > @count or pos < 1 then return false
+    @current = pos
+    slide_offset = -1 * @gallery_width() * (@current - 1)
+    thumb_offset = @calc_thumb_offset() if SETTINGS.thumbs
+    @load_slide_image()
+    @set_current_elements()
+    @set_navigation_hidden()
+    @offset_slide_image()
+    @update_caption_height()
+    @update_slide_count()
+    @slide_wrappers().each (i,e) -> $(e).css("left","#{slide_offset}px")
+    @thumb_wrapper().css("left","#{thumb_offset}px") if SETTINGS.thumbs
+    window.location.hash = "#slide-#{@current}"
+  
+  resize: (fullscreen=false)->
+    width = @gallery_width()
+    
+    @wrapper.addClass("is-resizing")
+    clearTimeout(@resizing_timeout) if @resizing_timeout
+    @resizing_timeout = setTimeout(=> 
+      @wrapper.removeClass("is-resizing")
+    , 100)
+    
+    @wrapper.removeClass("fmg-gallery-tablet fmg-gallery-mobile fmg-gallery-fullscreen")
+    if fullscreen
+      @wrapper.addClass("fmg-gallery-fullscreen")
+      @wrapper.find(".fmg-viewport").height(@wrapper.height())
+    else
+      @wrapper.addClass("fmg-gallery-tablet") if width < SETTINGS.breakpoints.tablet and width >= SETTINGS.breakpoints.mobile
+      @wrapper.addClass("fmg-gallery-mobile") if width < SETTINGS.breakpoints.mobile
+      @wrapper.find(".fmg-viewport, .fmg-menu-item").removeAttr("style")
+ 
+    @wrapper.find(".fmg-slide, .fmg-caption").each -> $(this).width(width)  
+    @slide_wrappers().each (i,e) => $(e).width(width * @count)      
+    @thumb_wrapper().width(Math.ceil(SETTINGS.thumb_width * @count)) if SETTINGS.thumbs
+    @slide_to(@current)
+      
+
+  toggle_fullscreen: ->
+    @full_screen_enabled ?= false
+    @bind_fullscreen_change
+    if @full_screen_enabled
+      @exit_fullscreen()
+    else
+      @enter_fullscreen()
+      
+  enter_fullscreen: ->
+    el = @wrapper[0]
+    document.onwebkitfullscreenchange = @on_fullscreen_change
+    document.onmozfullscreenchange = @on_fullscreen_change
+    document.onfullscreenchange = @on_fullscreen_change
+    return el.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT) if el.webkitRequestFullscreen
+    return el.mozRequestFullScreen() if el.mozRequestFullScreen
+    el.requestFullscreen()
+    
+  exit_fullscreen: ->
+    document.cancelFullScreen = document.webkitExitFullscreen || document.mozCancelFullScreen || document.exitFullscreen
+    document.cancelFullScreen();
+      
+  on_fullscreen_change: =>
+    @full_screen_enabled = !@full_screen_enabled
+    @loading(true)
+    setTimeout =>
+      @loading(false)
+      @resize(@full_screen_enabled)
+    , 200
+    
+      
+  has_fullscreen: ->
+    return typeof document.fullscreenEnabled == "undefined" ? true : false
+
+  gallery_width: ->
+    @wrapper.width()  
+    
+  calc_thumb_offset: -> 
+    per_slide = current = 1
+    per_slide++ while (per_slide + 1) * SETTINGS.thumb_width <= @gallery_width()
+    while current <= Math.ceil(@count / per_slide)
+      start = ((current - 1) * per_slide) + 1 
+      end = current * per_slide
+      if @current >= start and @current <= end then break
+      current++
+    offset = SETTINGS.thumb_width * ((current - 1) * per_slide) * -1
+    @load_thumb_images(start,end)
+    offset
+    
+  offset_slide_image: ->
+    viewport_height = @wrapper.find(".fmg-viewport").height()
+    image = @wrapper.find(".fmg-slide:eq(#{@current-1}) img")
+    h = image.data("height")
+    w = image.data("width")
+    scale = @gallery_width() / w
+    scaled_image_height = Math.round(scale * h)
+    if @full_screen_enabled
+      image.css("height",h)
+      image.css("width",w)
+      h_diff = viewport_height - h
+      w_diff = @gallery_width() - w
+      scaled_diff = viewport_height - scaled_image_height
+      image.css("margin-top",h_diff/2) if h_diff > 0
+      image.css("height",viewport_height).css("width","") if h_diff < 0
+      image.css("height",scaled_image_height) if scaled_diff < 0 and scale < 1
+    else if scaled_image_height < viewport_height
+      diff = viewport_height - scaled_image_height
+      image.css("margin-top",diff/2)
+      image.css("height", scaled_image_height)
+      image.css("width","")
+    else
+      image.css("margin-top","")
+      image.css("height","")
+      image.css("width","")
+     
+      
+  update_slide_count: ->
+    @wrapper.find(".fmg-count-current").text(@current)
+    @wrapper.find(".fmg-count-total").text(@count)
+      
+  update_caption_height: ->
+    height = @wrapper.find(".fmg-caption:eq(#{@current-1})").height()
+    captions =  @wrapper.find(".fmg-captions")
+    captions.not(".is-hidden").height(height)
+    if @full_screen_enabled
+      caption_offset = if captions.hasClass("is-hidden") then 5 else height + 5
+      @wrapper.find(".fmg-menu-item").css("bottom",caption_offset)
+  
+  load_slide_image: ->
+    image = @wrapper.find(".fmg-slide:eq(#{@current-1}) img")
+    src = if @gallery_width() < SETTINGS.breakpoints.mobile and image.data("src-mobile") then "src-mobile" else "src"
+    src = "src-fullscreen" if @full_screen_enabled and image.data("src-fullscreen")
+    if image.data("src-loaded") == src then return
+    image.attr("src",image.data(src))
+    image.data("src-loaded",src)
+  
+  load_thumb_images: (start,end) ->
+    @wrapper.find(".fmg-thumbs img").each (i,e) =>
+      i++ # match up to start/end indexing
+      image = $(e)
+      if i >= start and i <= (end + 1) and !image.data("loaded")
+        image.attr("src",image.data("src"))
+        image.data("loaded", true)
+    
+  set_current_elements: ->
+    c = @current - 1 
+    @wrapper.find(".fmg-slide,.fmg-caption,.fmg-thumbs img").removeClass("is-current")
+    @wrapper.find(".fmg-slide:eq(#{c}),.fmg-caption:eq(#{c}),.fmg-thumbs img:eq(#{c})").addClass("is-current")
+    
+  set_navigation_hidden: ->
+    @wrapper.find(".fmg-viewport-nav").removeClass("is-hidden")
+    if @current == 1 then @wrapper.find(".fmg-viewport-nav-left").addClass("is-hidden") 
+    if @current == @count then @wrapper.find(".fmg-viewport-nav-right").addClass("is-hidden") 
+    
+  resolve_current: ->
+    if(window.location.hash == "" || window.location.hash.indexOf("#slide-") == -1) then return 1
+    parseInt(window.location.hash.split("#slide-",2)[1]) || 1;
+  
+  loading: (state) ->
+    if state  then @wrapper.addClass("is-loading") else @wrapper.removeClass("is-loading")
+  
+  add_listener: (event,fn) ->
+    @events or= []
+    @events[event] = [] unless @events[event]
+    @events[event].push(fn)
+  
+  trigger: (event,args) ->
+    (cb(args) for cb in @events[event]) if @events[event]
+  
+new Gallery("#gallery_one")
+
+# TODO ----------------
+# full screen
+# bind to touch start on mobile
+# add swipe gesture
+# mobile: add class so hover states are always visible
+# bug onresize to large
+# pre-post slide
+
+# integration: 
+# photo meta
+
+
+# DONE ---- 
+# ability to hide captions - DONE
+# ability to hide thumbs - DONE
+# resize images based on width - DONE
+# only load images are they appear - DONE
+# different images for desktop and mobile - DONE
+# get basic styles in and content in - DONE 
+# basic slide transition for one width - DONE
+# distance to slide based on width of article - DONE
+# basic slider transition for all widths - DONE
+# all things slide at once - DONE
+# DONE - resize listener
---
+++ gallery.js
@@ -0,0 +1,330 @@
+(function() {
+  var Gallery;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  Gallery = (function() {
+    var SETTINGS;
+    SETTINGS = {
+      thumbs: true,
+      thumb_width: 150,
+      captions: true,
+      meta: true,
+      breakpoints: {
+        tablet: 940,
+        mobile: 768
+      },
+      heights: {
+        desktop: 550,
+        tablet: 400,
+        mobile: 250
+      }
+    };
+    function Gallery(id, config) {
+      if (config == null) {
+        config = {};
+      }
+      this.on_fullscreen_change = __bind(this.on_fullscreen_change, this);
+      this.wrapper = $(id);
+      this.current = this.resolve_current();
+      this.count = this.wrapper.find(".fmg-slide").length;
+      this.loading(true);
+      this.init_bindings();
+      this.resize();
+      this.loading(false);
+    }
+    Gallery.prototype.slide_wrappers = function() {
+      return this.wrapper.find(".fmg-slides-wrapper, .fmg-captions-wrapper");
+    };
+    Gallery.prototype.thumb_wrapper = function() {
+      return this.wrapper.find(".fmg-thumbs-wrapper").first();
+    };
+    Gallery.prototype.init_bindings = function() {
+      $(document).on("click", ".fmg-viewport-nav-left", __bind(function() {
+        this.slide_to(this.current - 1);
+        return false;
+      }, this));
+      $(document).on("click", ".fmg-viewport-nav-right", __bind(function() {
+        this.slide_to(this.current + 1);
+        return false;
+      }, this));
+      $(document).on("click", ".fmg-thumbs-wrapper img", __bind(function(e) {
+        var index;
+        index = $(".fmg-thumbs-wrapper img").index(e.currentTarget);
+        this.slide_to(index + 1);
+        return false;
+      }, this));
+      if (!this.has_fullscreen()) {
+        $(".fmg-fullscreen").remove();
+      }
+      $(document).on("click", ".fmg-fullscreen", __bind(function() {
+        this.toggle_fullscreen();
+        return false;
+      }, this));
+      $(document).on("click", ".fmg-toggle-thumbs", __bind(function() {
+        this.wrapper.find(".fmg-thumbs").toggleClass("is-hidden");
+        return false;
+      }, this));
+      $(document).on("click", ".fmg-toggle-captions", __bind(function() {
+        this.wrapper.find(".fmg-captions").toggleClass("is-hidden");
+        return false;
+      }, this));
+      $(document).keydown(__bind(function(e) {
+        if (e.keyCode === 37) {
+          this.slide_to(this.current - 1);
+        }
+        if (e.keyCode === 39) {
+          return this.slide_to(this.current + 1);
+        }
+      }, this));
+      return window.onresize = __bind(function() {
+        return this.resize();
+      }, this);
+    };
+    Gallery.prototype.slide_to = function(pos) {
+      var slide_offset, thumb_offset;
+      if (pos > this.count || pos < 1) {
+        return false;
+      }
+      this.current = pos;
+      slide_offset = -1 * this.gallery_width() * (this.current - 1);
+      if (SETTINGS.thumbs) {
+        thumb_offset = this.calc_thumb_offset();
+      }
+      this.load_slide_image();
+      this.set_current_elements();
+      this.set_navigation_hidden();
+      this.offset_slide_image();
+      this.update_caption_height();
+      this.update_slide_count();
+      this.slide_wrappers().each(function(i, e) {
+        return $(e).css("left", "" + slide_offset + "px");
+      });
+      if (SETTINGS.thumbs) {
+        this.thumb_wrapper().css("left", "" + thumb_offset + "px");
+      }
+      return window.location.hash = "#slide-" + this.current;
+    };
+    Gallery.prototype.resize = function(fullscreen) {
+      var width;
+      if (fullscreen == null) {
+        fullscreen = false;
+      }
+      width = this.gallery_width();
+      this.wrapper.addClass("is-resizing");
+      if (this.resizing_timeout) {
+        clearTimeout(this.resizing_timeout);
+      }
+      this.resizing_timeout = setTimeout(__bind(function() {
+        return this.wrapper.removeClass("is-resizing");
+      }, this), 100);
+      this.wrapper.removeClass("fmg-gallery-tablet fmg-gallery-mobile fmg-gallery-fullscreen");
+      if (fullscreen) {
+        this.wrapper.addClass("fmg-gallery-fullscreen");
+        this.wrapper.find(".fmg-viewport").height(this.wrapper.height());
+      } else {
+        if (width < SETTINGS.breakpoints.tablet && width >= SETTINGS.breakpoints.mobile) {
+          this.wrapper.addClass("fmg-gallery-tablet");
+        }
+        if (width < SETTINGS.breakpoints.mobile) {
+          this.wrapper.addClass("fmg-gallery-mobile");
+        }
+        this.wrapper.find(".fmg-viewport, .fmg-menu-item").removeAttr("style");
+      }
+      this.wrapper.find(".fmg-slide, .fmg-caption").each(function() {
+        return $(this).width(width);
+      });
+      this.slide_wrappers().each(__bind(function(i, e) {
+        return $(e).width(width * this.count);
+      }, this));
+      if (SETTINGS.thumbs) {
+        this.thumb_wrapper().width(Math.ceil(SETTINGS.thumb_width * this.count));
+      }
+      return this.slide_to(this.current);
+    };
+    Gallery.prototype.toggle_fullscreen = function() {
+      var _ref;
+      if ((_ref = this.full_screen_enabled) == null) {
+        this.full_screen_enabled = false;
+      }
+      this.bind_fullscreen_change;
+      if (this.full_screen_enabled) {
+        return this.exit_fullscreen();
+      } else {
+        return this.enter_fullscreen();
+      }
+    };
+    Gallery.prototype.enter_fullscreen = function() {
+      var el;
+      el = this.wrapper[0];
+      document.onwebkitfullscreenchange = this.on_fullscreen_change;
+      document.onmozfullscreenchange = this.on_fullscreen_change;
+      document.onfullscreenchange = this.on_fullscreen_change;
+      if (el.webkitRequestFullscreen) {
+        return el.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+      }
+      if (el.mozRequestFullScreen) {
+        return el.mozRequestFullScreen();
+      }
+      return el.requestFullscreen();
+    };
+    Gallery.prototype.exit_fullscreen = function() {
+      document.cancelFullScreen = document.webkitExitFullscreen || document.mozCancelFullScreen || document.exitFullscreen;
+      return document.cancelFullScreen();
+    };
+    Gallery.prototype.on_fullscreen_change = function() {
+      this.full_screen_enabled = !this.full_screen_enabled;
+      this.loading(true);
+      return setTimeout(__bind(function() {
+        this.loading(false);
+        return this.resize(this.full_screen_enabled);
+      }, this), 200);
+    };
+    Gallery.prototype.has_fullscreen = function() {
+      var _ref;
+      return (_ref = typeof document.fullscreenEnabled === "undefined") != null ? _ref : {
+        "true": false
+      };
+    };
+    Gallery.prototype.gallery_width = function() {
+      return this.wrapper.width();
+    };
+    Gallery.prototype.calc_thumb_offset = function() {
+      var current, end, offset, per_slide, start;
+      per_slide = current = 1;
+      while ((per_slide + 1) * SETTINGS.thumb_width <= this.gallery_width()) {
+        per_slide++;
+      }
+      while (current <= Math.ceil(this.count / per_slide)) {
+        start = ((current - 1) * per_slide) + 1;
+        end = current * per_slide;
+        if (this.current >= start && this.current <= end) {
+          break;
+        }
+        current++;
+      }
+      offset = SETTINGS.thumb_width * ((current - 1) * per_slide) * -1;
+      this.load_thumb_images(start, end);
+      return offset;
+    };
+    Gallery.prototype.offset_slide_image = function() {
+      var diff, h, h_diff, image, scale, scaled_diff, scaled_image_height, viewport_height, w, w_diff;
+      viewport_height = this.wrapper.find(".fmg-viewport").height();
+      image = this.wrapper.find(".fmg-slide:eq(" + (this.current - 1) + ") img");
+      h = image.data("height");
+      w = image.data("width");
+      scale = this.gallery_width() / w;
+      scaled_image_height = Math.round(scale * h);
+      if (this.full_screen_enabled) {
+        image.css("height", h);
+        image.css("width", w);
+        h_diff = viewport_height - h;
+        w_diff = this.gallery_width() - w;
+        scaled_diff = viewport_height - scaled_image_height;
+        if (h_diff > 0) {
+          image.css("margin-top", h_diff / 2);
+        }
+        if (h_diff < 0) {
+          image.css("height", viewport_height).css("width", "");
+        }
+        if (scaled_diff < 0 && scale < 1) {
+          return image.css("height", scaled_image_height);
+        }
+      } else if (scaled_image_height < viewport_height) {
+        diff = viewport_height - scaled_image_height;
+        image.css("margin-top", diff / 2);
+        image.css("height", scaled_image_height);
+        return image.css("width", "");
+      } else {
+        image.css("margin-top", "");
+        image.css("height", "");
+        return image.css("width", "");
+      }
+    };
+    Gallery.prototype.update_slide_count = function() {
+      this.wrapper.find(".fmg-count-current").text(this.current);
+      return this.wrapper.find(".fmg-count-total").text(this.count);
+    };
+    Gallery.prototype.update_caption_height = function() {
+      var caption_offset, captions, height;
+      height = this.wrapper.find(".fmg-caption:eq(" + (this.current - 1) + ")").height();
+      captions = this.wrapper.find(".fmg-captions");
+      captions.not(".is-hidden").height(height);
+      if (this.full_screen_enabled) {
+        caption_offset = captions.hasClass("is-hidden") ? 5 : height + 5;
+        return this.wrapper.find(".fmg-menu-item").css("bottom", caption_offset);
+      }
+    };
+    Gallery.prototype.load_slide_image = function() {
+      var image, src;
+      image = this.wrapper.find(".fmg-slide:eq(" + (this.current - 1) + ") img");
+      src = this.gallery_width() < SETTINGS.breakpoints.mobile && image.data("src-mobile") ? "src-mobile" : "src";
+      if (this.full_screen_enabled && image.data("src-fullscreen")) {
+        src = "src-fullscreen";
+      }
+      if (image.data("src-loaded") === src) {
+        return;
+      }
+      image.attr("src", image.data(src));
+      return image.data("src-loaded", src);
+    };
+    Gallery.prototype.load_thumb_images = function(start, end) {
+      return this.wrapper.find(".fmg-thumbs img").each(__bind(function(i, e) {
+        var image;
+        i++;
+        image = $(e);
+        if (i >= start && i <= (end + 1) && !image.data("loaded")) {
+          image.attr("src", image.data("src"));
+          return image.data("loaded", true);
+        }
+      }, this));
+    };
+    Gallery.prototype.set_current_elements = function() {
+      var c;
+      c = this.current - 1;
+      this.wrapper.find(".fmg-slide,.fmg-caption,.fmg-thumbs img").removeClass("is-current");
+      return this.wrapper.find(".fmg-slide:eq(" + c + "),.fmg-caption:eq(" + c + "),.fmg-thumbs img:eq(" + c + ")").addClass("is-current");
+    };
+    Gallery.prototype.set_navigation_hidden = function() {
+      this.wrapper.find(".fmg-viewport-nav").removeClass("is-hidden");
+      if (this.current === 1) {
+        this.wrapper.find(".fmg-viewport-nav-left").addClass("is-hidden");
+      }
+      if (this.current === this.count) {
+        return this.wrapper.find(".fmg-viewport-nav-right").addClass("is-hidden");
+      }
+    };
+    Gallery.prototype.resolve_current = function() {
+      if (window.location.hash === "" || window.location.hash.indexOf("#slide-") === -1) {
+        return 1;
+      }
+      return parseInt(window.location.hash.split("#slide-", 2)[1]) || 1;
+    };
+    Gallery.prototype.loading = function(state) {
+      if (state) {
+        return this.wrapper.addClass("is-loading");
+      } else {
+        return this.wrapper.removeClass("is-loading");
+      }
+    };
+    Gallery.prototype.add_listener = function(event, fn) {
+      this.events || (this.events = []);
+      if (!this.events[event]) {
+        this.events[event] = [];
+      }
+      return this.events[event].push(fn);
+    };
+    Gallery.prototype.trigger = function(event, args) {
+      var cb, _i, _len, _ref, _results;
+      if (this.events[event]) {
+        _ref = this.events[event];
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          cb = _ref[_i];
+          _results.push(cb(args));
+        }
+        return _results;
+      }
+    };
+    return Gallery;
+  })();
+  new Gallery("#gallery_one");
+}).call(this);
